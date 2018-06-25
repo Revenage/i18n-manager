724b9b4e5108fa2e4f1c4edd893feae2dbed047c
Revenage <Revenage@users.noreply.github.com>
2018-06-25 16:11:34 +0300

    fix static

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app.wsgi
+web: python manage.py collectstatic --noinput; web: gunicorn app.wsgi
--- app/settings.py
+++ app/settings.py
@@ -134,12 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../i18n-manager/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 AUTH_USER_MODEL = 'users.CustomUser'
