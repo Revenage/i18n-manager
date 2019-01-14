69583a4dfe407e68a2fb0315f72ad13edf43a2fa
Revenage <Revenage@users.noreply.github.com>
2019-01-14 08:52:09 +0200

    back to proj

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-ORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'localhost:8000',
