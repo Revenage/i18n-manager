4289fbd345738215f9553e893cf439f3e0861489
Roman Volobuiev <roman.volobuev@es.bet>
2019-01-27 22:25:12 +0200

    up manage.py

--- manage.py
+++ manage.py
@@ -5,8 +5,10 @@ import sys
 import dotenv
 
 if __name__ == "__main__":
-    dotenv.read_dotenv(os.path.join(
-        os.path.dirname(__file__), '.env'))
+    try:
+        dotenv.read_dotenv(os.path.join(
+            os.path.dirname(__file__), '.env'))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
     try:
         from django.core.management import execute_from_command_line
